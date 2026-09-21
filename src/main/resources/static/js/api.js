30682e926a2fd7c118fddadecf580f70c9948a5c
Dragon <dragon@vinova.com.sg>
2026-09-21 16:49:03 +0700

    feat: search list Faculty  & student

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,41 +1,49 @@
+const API = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
 const userApi = {
   list(keyword = "") {
-    return APIInstance.get("/users", { params: { keyword } });
+    return API.get("/users", { params: { keyword } });
   },
   detail(id) {
-    return APIInstance.get(`/users/${id}`);
+    return API.get(`/users/${id}`);
   },
   create(data) {
-    return APIInstance.post("/users", data);
+    return API.post("/users", data);
   },
   update(id, data) {
-    return APIInstance.put(`/users/${id}`, data);
+    return API.put(`/users/${id}`, data);
   },
   remove(id) {
-    return APIInstance.delete(`/users/${id}`);
+    return API.delete(`/users/${id}`);
   },
 };
 
 const facultyApi = {
   list() {
-    return APIInstance.get("/faculties");
+    return API.get("/faculties");
   },
 };
 
 const studentApi = {
   list(facultyId, keyword = "") {
-    return APIInstance.get(`/faculties/${facultyId}/students`, { params: { keyword } });
+    return API.get(`/faculties/${facultyId}/students`, { params: { keyword } });
   },
   detail(facultyId, studentId) {
-    return APIInstance.get(`/faculties/${facultyId}/students/${studentId}`);
+    return API.get(`/faculties/${facultyId}/students/${studentId}`);
   },
   create(facultyId, data) {
-    return APIInstance.post(`/faculties/${facultyId}/students`, data);
+    return API.post(`/faculties/${facultyId}/students`, data);
   },
   update(facultyId, studentId, data) {
-    return APIInstance.put(`/faculties/${facultyId}/students/${studentId}`, data);
+    return API.put(`/faculties/${facultyId}/students/${studentId}`, data);
   },
   remove(facultyId, studentId) {
-    return APIInstance.delete(`/faculties/${facultyId}/students/${studentId}`);
+    return API.delete(`/faculties/${facultyId}/students/${studentId}`);
   },
 };
--- src/main/resources/static/js/axios-config.js
+++
@@ -1,6 +0,0 @@
-const APIInstance = axios.create({
-  baseURL: "/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
